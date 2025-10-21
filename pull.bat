4d1dc8bd7b834a97b6e25bf8c3bab7a5859a3192
dolkiztnbb <freemoneyhack2025@gmail.com>
2025-10-21 22:09:07 +0300

    solevye dobavochki

---
+++ pull.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ============clenup
+del /f /s /q .git
+echo ============init
+git init
+echo ============remote job
+git remote add origin https://github.com/rewnant/kurto4ka.git
+git remote -v
+echo ============branching
+git branch
+echo ============pulling
+git add .
+git pull origin dev
+git checkout dev
