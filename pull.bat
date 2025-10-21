f2b675faead3536c7933819e8071e90689872f9b
dolkiztnbb <freemoneyhack2025@gmail.com>
2025-10-21 22:11:05 +0300

    A

--- pull.bat
+++ pull.bat
@@ -6,8 +6,6 @@ git init
 echo ============remote job
 git remote add origin https://github.com/rewnant/kurto4ka.git
 git remote -v
-echo ============branching
-git branch
 echo ============pulling
 git add .
 git pull origin dev
